7ed4c9245068c42c342431f51c0f6ef7956d9323
sebastien.hutinet <sebastien.hutinet@gmail.com>
2022-01-28 12:19:10 +0100

    launch hidden console + add pb

--- click_me.bat
+++
@@ -1,1 +0,0 @@
-START /MIN CMD.EXE /C R-Portable\bin\Rscript.exe --vanilla rscript.R 1>workflow.lipido.log 2>&1
--- rscript.R
+++ rscript.R
@@ -1,9 +1,18 @@
+pb <- utils::winProgressBar(
+	title = sprintf("Starting app"),
+	label = "Initializing ...")
+
 Sys.setenv(RENV_DOWNLOAD_FILE_METHOD = "libcurl")
 options(stringsAsFactors = FALSE)
 options(browser = tools::file_path_as_absolute("Chromium/Chrome.exe"))
 options(show.error.messages = TRUE)
 
+utils::setWinProgressBar(pb, 0, label = "Activate application ...")  
 source("renv/activate.R")
-renv::restore(prompt = FALSE)
+utils::setWinProgressBar(pb, value = .25, 
+    label = "Restore library...(could take some time if first time)")  
+renv::restore(library = "R-Portable/library", clean = TRUE, prompt = FALSE)
+utils::setWinProgressBar(pb, value = 1, label = "Loading app...")  
 devtools::load_all("workflow.lipido")
+close(pb)
 run_shiny_app(NULL)
