62554ba5d0dcece19166760c916669254e99f978
sebastien.hutinet <sebastien.hutinet@gmail.com>
2022-02-11 12:12:55 +0100

    update launch of app

--- rscript.R
+++ rscript.R
@@ -3,16 +3,17 @@ pb <- utils::winProgressBar(
 	label = "Initializing ...")
 
 Sys.setenv(RENV_DOWNLOAD_FILE_METHOD = "libcurl")
+Sys.setenv(PATH = paste(tools::file_path_as_absolute("Rtools\\bin"), Sys.getenv("PATH"), sep = ";"))
 options(stringsAsFactors = FALSE)
-options(browser = tools::file_path_as_absolute("Chromium/Chrome.exe"))
 options(show.error.messages = TRUE)
 
-utils::setWinProgressBar(pb, 0, label = "Activate application ...")  
+utils::setWinProgressBar(pb, 0, label = "Activate application ...")
 source("renv/activate.R")
-utils::setWinProgressBar(pb, value = .25, 
-    label = "Restore library...(could take some time if first time)")  
+renv::upgrade(prompt = FALSE)
+utils::setWinProgressBar(pb, value = .25,
+    label = "Restore library...(could take some time if first time)")
 renv::restore(library = "R-Portable/library", clean = TRUE, prompt = FALSE)
-utils::setWinProgressBar(pb, value = 1, label = "Loading app...")  
+utils::setWinProgressBar(pb, value = 1, label = "Loading app...")
 devtools::load_all("workflow.lipido")
 close(pb)
-run_shiny_app(NULL)
+run_shiny_app(converter = "pwiz/msconvert.exe")
